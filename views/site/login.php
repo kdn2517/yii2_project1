bb5fadd541033934a7ac19ece8c041d556d58d1a
kdn2517 <rab7777777rab@gmail.com>
2018-03-02 12:42:58 +0300

    завершена регистрация (последний штрих - убрал "запомнить меня")

--- views/site/login.php
+++ views/site/login.php
@@ -28,9 +28,9 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <?= $form->field($model, 'password')->passwordInput() ?>
 
-        <?= $form->field($model, 'rememberMe')->checkbox([
+<!--        <?= $form->field($model, 'rememberMe')->checkbox([
             'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
+        ]) ?> -->
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
